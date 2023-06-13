8aa904b22c804c8cfe279bbb6c86332267f0c4fe
Todd O'Neil <todd.oneil07@gmail.com>
2023-06-13 12:27:02 -0400

    added database connection class

---
+++ Data/IDbContext.cs
@@ -0,0 +1,8 @@
+﻿namespace OrdersApplication.Data
+{
+    public interface IDbContext
+    {
+        Task<IEnumerable<T>> GetDataWithQuery<T, P>(string Query, P parameters, string connectionId = "DefaultConnection");
+        Task SaveData<T>(string query, T parameters, string connectionId = "DefaultConnection");
+    }
+}
--- Data/dbContext.cs
+++ Data/dbContext.cs
@@ -1,6 +1,35 @@
-﻿namespace OrdersApplication.Data
+﻿namespace OrdersApplication.Data;
+using MySql.Data;
+using Dapper;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+public class DbContext : IDbContext
 {
-    public class dbContext
+    private readonly IConfiguration _configuration;
+    public DbContext(IConfiguration config)
     {
+        _configuration = config;
     }
+
+    public async Task<IEnumerable<T>> GetDataWithQuery<T, P>(
+        string Query,
+        P parameters,
+        string connectionId = "DefaultConnection")
+    {
+        using IDbConnection connection = new MySqlConnection(_configuration.GetConnectionString(connectionId));
+
+        return await connection.QueryAsync<T>(Query, parameters, commandType: CommandType.Text);
+    }
+
+    public async Task SaveData<T>(
+        string query,
+        T parameters,
+        string connectionId = "DefaultConnection")
+    {
+        using IDbConnection connection = new MySqlConnection(_configuration.GetConnectionString(connectionId));
+
+        await connection.ExecuteAsync(query, parameters, transaction: connection.BeginTransaction(), commandType: CommandType.Text);
+    }
+
 }
